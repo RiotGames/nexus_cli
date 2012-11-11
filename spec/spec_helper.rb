7dacc58c6732bd4fd2d0694636997f6d2941fea6
Jamie Winsor <jamie@vialstudios.com>
2012-11-10 16:46:37 -0800

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 APP_ROOT = File.expand_path('../../', __FILE__)
 
-ENV["NEXUS_CONFIG"] = File.join(APP_ROOT, "spec", "fixtures", "nexus.config")
+ENV["NEXUS_CONFIG"] = File.expand_path(File.join(APP_ROOT, "spec", "fixtures", "nexus.config"))
 
 require 'nexus_cli'
 require 'webmock/rspec'
